986f2bae67ecb8c27b49aad2adc7c489f08b0046
Peter Reutemann <fracpete@gmail.com>
2020-05-01 11:30:48 +1200

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
 # parsergen-maven-plugin
-Maven plugin for generating parsers using JavaCup and JFlex
+Maven plugin for generating parsers using JavaCup and JFlex.
+
+Currently based on:
+* [JavaCup](http://www2.cs.tum.edu/projects/cup/): 11b-20160615
+* [JFlex](https://jflex.de/): 1.4.3
+
+
+## Example
+
+Assuming you have your `Scanner.flex` and `Parser.cup` files in the following 
+directory:
+
+```
+src/main/resources/my/project/parser
+```
+
+Then use the following plugin definition in the `build` section of you your 
+`pom.xml` file:
+
+```xml
+  <build>
+    <plugins>
+      <plugin>
+        <groupId>com.github.fracpete</groupId>
+        <artifactId>parsergen-maven-plugin</artifactId>
+        <version>0.0.1</version>
+        <configuration>
+          <directories>
+            <directory>${project.basedir}/src/main/resources/my/project/parser</directory>
+          </directories>
+        </configuration>
+      </plugin>
+    </plugins>
+  </build>
+```
+
+Using `mvn parsergen:build` will place the generated Java code of the parser
+in package `my.project.parser` (the corresponding directory to `resources/my/project/parser`
+is `java/my/project/parser`).
+
+**Note:** The `directory` tag can be supplied multiple times, in case you need
+to compile multiple parsers.
---
+++ src/main/java/com/github/fracpete/parsergen/ParserGenMojo.java
@@ -0,0 +1,83 @@
+package com.github.fracpete.parsergen;
+
+/*
+ * Copyright 2020 University of Waikato, Hamilton, NZ.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * Mojo for parser generation using JavaCup and JFlex.
+ *
+ * @author FracPete (fracpete at waikato dot ac dot nz)
+ * @goal build
+ * @phase process-resources
+ */
+public class ParserGenMojo extends AbstractMojo {
+
+  /**
+   * @parameter
+   * @required
+   */
+  protected String[] directories;
+
+  public void execute() throws MojoExecutionException, MojoFailureException {
+    String[] options;
+    File dir;
+
+    for (String directory: directories) {
+      dir = new File(directory);
+      getLog().info("Processing directory: " + dir);
+
+      try {
+        if (!dir.exists())
+          throw new IOException("Directory does not exist: " + dir);
+
+        // JFlex
+        options = new String[]{
+          "--jlex",
+          "--quiet",
+          "--nobak",
+          "--outdir",
+          dir.getAbsolutePath().replace("resources", "java"),
+          dir.getAbsolutePath() + "/Scanner.jflex"
+        };
+        JFlex.Main.main(options);
+
+        // java-cup
+        options = new String[]{
+          "-parser",
+          "Parser",
+          "-interface",
+          "-nosummary",
+          "-destdir",
+          dir.getAbsolutePath().replace("resources", "java"),
+          dir.getAbsolutePath() + "/Parser.cup"
+        };
+        java_cup.Main.main(options);
+      }
+      catch (Exception e) {
+        getLog().error(e);
+        throw new MojoFailureException(e.getMessage());
+      }
+    }
+  }
+}
